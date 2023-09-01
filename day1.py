5010fc60588608d938fba410c5f3f2b4b0f2f2bb
huzhifeng <1411806197@qq.com>
2023-09-01 14:25:35 +0800

    dayq_

--- day1.py
+++ day1.py
@@ -13,7 +13,7 @@
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Union[str, None] = None):
 #     return {"item_id": item_id, "q": q}
-
+import uvicorn
 from fastapi import FastAPI
 
 
@@ -31,3 +31,18 @@ def get_sum():
 @app.get('/items/{items_id}')
 async def get_ttt(items_id:int):
     return {'ffff':items_id}
+
+
+
+
+# 顺序问题
+@app.get('/iss/{iss_id}')
+async def get_iss(iss_id:str):
+    return {'kkkk':iss_id}
+
+@app.get('/iss/mer')
+async def gu_iss():
+    return 'lpppp_hello'
+
+if __name__ == '__main__':
+    uvicorn.run('day1:app',host='127.0.0.1',port=8000,reload=True)
